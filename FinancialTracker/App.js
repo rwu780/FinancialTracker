4f0f235f4c86296f092342446cd7268301a9d0ea
rwu780 <rwu4@ualberta.ca>
2018-03-13 15:55:39 -0600

    Static view completed, having trouble read and writing to firebase

--- FinancialTracker/App.js
+++ FinancialTracker/App.js
@@ -12,95 +12,154 @@ import {
   View,
   TouchableHighlight,
   FlatList,
+  TouableOpacity,
+  Button,
 } from 'react-native';
 
 import * as firebase from 'firebase';
 
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBp77ueoIxjvq_bibnMOYAGWLM6RSmLlOg",
   authDomain: "financial-tracker-e6124.firebaseapp.com",
-  databaseURL: "https://financial-tracker-e6124.firebaseio.com/",
-  storageBucket: "",
-};
+  databaseURL: "https://financial-tracker-e6124.firebaseio.com",
+  projectId: "financial-tracker-e6124",
+  storageBucket: "financial-tracker-e6124.appspot.com",
+  messagingSenderId: "445078506224"
+}
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props){
     super(props);
+    this.itemsRef = firebaseApp.database().ref();
+    this.itemsRef.on('value', function(snapshot){
+    var items = [];
+    console.log("Hello there");
+    snapshow.forEach((child) => {
+      items.push({
+        name: child.val().title,
+        _key: child.val().key,
+        amount: child.val().amount,
+      });
+      firebaseApp.database().ref('users').set({
+        username: "ABC",
+      });
+
+    });
+  });
+  };
+
+
+
+  listenForIncomes(itemsRef){
+    itemsRef.on('value', (snap) =>{
+      var items=[];
+      snap.forEach((child) => {
+        items.push({
+          name: child.val().title,
+          _key: child.key,
+          amount: child.amount,
+        });
+      });
+      this.incomes = items;
+    })
+  }
+
+  renderItem({item, index}){
+    return(
+      <TouchableHighlight onPress={this.itemPress}>
+        <View style = {styles.li}>
+          <Text style={styles.liText}>{item.name} {item.amount}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  itemPress(){
+
+  }
+  //
+  // addEntry(){
+  //   AlertIOS.prompt(
+  //     'Enter a value',
+  //     null,
+  //     text=> console.log("You entered" + text);
+  //   )
+  // }
+
+
+  state = {
+    incomes: [
+      {
+        name:"Salaries",
+        amount:3000,
+      },
+      {
+        name:"Investment",
+        amount: 350,
+      }
+    ],
+    expenses: [
+      {
+        name:"Utilities",
+        amount: 200,
+      },
+      {
+        name:"Phone and Cable",
+        amount: 150,
+      },
+    ]
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar title="Financial Tracker" />
+        <HeaderBar title="Financial Tracker" />
+        <DividerBar title="Income" />
         <FlatList
-          data={[
-            {title: 'Devin', key:"Devin", price:"300"},
-            {title: 'Jackson', key: 'Jackson', price:"(100)"},
-            {title: 'James', key: 'James', price:"(200)"},
-            {title: 'Joel', key:'Joel', price:""},
-            {title: 'John', key:"John", price:""},
-          ]}
-          renderItem={({item}) => <View><Text>{item.title} {item.price}</Text></View>}        />
-        <ActionButton title = "Add" />
+          data={this.incomes}
+          renderItem={this.renderItem}
+        >
+        </FlatList>
+
+        <DividerBar title="Expense" />
+        <FlatList
+          data={this.state.expenses}
+          renderItem={this.renderItem}
+        >
+        </FlatList>
+        <DividerBar title="Calculate" />
       </View>
     );
   }
 }
 
-class ActionButton extends Component {
-  render(){
-    return(
-      <View style={styles.action}>
-        <TouchableHighlight
-          underlayColor={constants.actionColor}
-          onPress={this.props.onPress}>
-          <Text style={styles.actionText}>{this.props.title}</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-}
-
-class StatusBar extends Component<props> {
+// StatusBar Component
+class HeaderBar extends Component<props> {
   render(){
     return(
       <View>
-        <View style={styles.statusbar}/>
+        <View style={styles.headerbar}/>
         <View style={styles.navbar}>
           <Text style={styles.navbarTitle}>{this.props.title}</Text>
+          <Button
+          onPress={this.props.addEntry}
+          title="Add Entry"
+          color="#841584"
+          />
         </View>
       </View>
     );
   }
 }
 
-class ListItem extends Component {
+class DividerBar extends Component<props>{
   render(){
-    return (
-      <TouchableHighlight onPress={this.props.onPress}>
-        <View style={styles.li}>
-          <Text style={styles.liText}>{this.props.item.title}</Text>
-        </View>
-      </TouchableHighlight>
-    );
-  }
-}
-
-class PlaceHolder extends Component<props>{
-  render(){
-    return(  
-      <View style={styles.navbar}>
+    return(
+      <View style={styles.dividerbar}>
         <Text style={styles.navbarTitle}>{this.props.title}</Text>
       </View>
     );
@@ -115,11 +174,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
   listview:{
     flex: 1,
@@ -155,7 +209,16 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "500"
   },
-  statusbar: {
+  dividerbar:{
+    alignItems:'center',
+    backgroundColor: '#fff',
+    borderColor: '#eee',
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 40,
+    flexDirection: 'row',
+  },
+  headerbar: {
     backgroundColor: '#fff',
     height: 22,
   },
